c24cf51070d1b91acaf9c80a88d815f16da0e81a
Kidus Yosef <kidusyosef1001@gmail.com>
2026-01-21 23:57:16 +0300

    3 | T - O(n) | S - O(n)

--- 2026/1. Jan/21/3.py
+++ 2026/1. Jan/21/3.py
@@ -12,19 +12,33 @@
 
 
 # A bit refinement in the res and ress handling
+# class Solution(object):
+#     def lengthOfLongestSubstring(self, s):
+#         n = len(s)
+#         res = 0
+        
+#         for i in range(n):
+#             visited = set()
+#             for j in range(i, n):
+#                 if s[j] not in visited:
+#                     visited.add(s[j])
+#                     res = max(res, j-i+1)
+#                 else:
+#                     break
+#         return res
+
 class Solution(object):
     def lengthOfLongestSubstring(self, s):
+        l = 0
+        visited = set()
         n = len(s)
         res = 0
-        
-        for i in range(n):
-            visited = set()
-            for j in range(i, n):
-                if s[j] not in visited:
-                    visited.add(s[j])
-                    res = max(res, j-i+1)
-                else:
-                    break
+        for r in range (n):
+            while s[r] in visited:
+                visited.remove(s[l])
+                l+=1
+            visited.add(s[r])
+            res = max(res, r-l+1)
         return res
         """
         :type s: str
